e507bc7175d7d3381524656ee9b67b4b19007ffb
jiachen <liujiachen9702@gmail.com>
2019-06-19 15:30:36 +1000

    8080 to 9090

--- src/main/java/com/xjbx/xjbx/XjbxApplication.java
+++ src/main/java/com/xjbx/xjbx/XjbxApplication.java
@@ -2,9 +2,24 @@ package com.xjbx.xjbx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class XjbxApplication {
+
+    @Bean
+    public TomcatServletWebServerFactory tomcat() {
+        TomcatServletWebServerFactory tomcatFactory = new TomcatServletWebServerFactory();
+        tomcatFactory.setPort(8090);
+        return tomcatFactory;
+    }
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(XjbxApplication.class, args);
